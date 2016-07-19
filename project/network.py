7a2237fe841966771e7df33ae89b7205b8658aa5
Alireza Sadeghi <alireza.sadeghi@pushe.co>
2016-07-20 02:23:04 +0430

    Report 4 added

--- project/network.py
+++ project/network.py
@@ -1,24 +1,22 @@
 from keras.layers import Dense, Input, Lambda
 from keras.models import Sequential, Model
 import keras.backend as K
-__author__ = 'mohsen'
-
 
 doc_dim = 300  # 300 is dummy
 hidden_dim = 128
+
 
 def euclidean_distance(vects):
     x, y = vects
     return K.sqrt(K.sum(K.square(x - y), axis=1, keepdims=True))
 
+
 def eucl_dist_output_shape(shapes):
     shape1, shape2 = shapes
-    return (shape1[0], 1)
+    return shape1[0], 1
 
 
 def create_base_network(input_dim):
-    '''Base network to be shared (eq. to feature extraction).
-    '''
     # TODO: replace with an RNN
     seq = Sequential()
     seq.add(Dense(128, input_shape=(input_dim,), activation='relu'))
@@ -37,6 +35,7 @@ def non_sim_loss(y_true, y_pred):
     margin = hidden_dim
     return K.square(K.maximum(margin - y_pred, 0))
 
+
 # assume number of related docs = 3 also add 1 irrelevant docs
 main_in = Input((doc_dim,))
 sim1_in = Input((doc_dim,))
@@ -52,11 +51,14 @@ topic_sim2 = base_net(sim2_in)
 topic_sim3 = base_net(sim3_in)
 topic_nonsim1 = base_net(nonsim1_in)
 
-
-dist1 = Lambda(euclidean_distance, output_shape=eucl_dist_output_shape)([topic_main, topic_sim1])
-dist2 = Lambda(euclidean_distance, output_shape=eucl_dist_output_shape)([topic_main, topic_sim2])
-dist3 = Lambda(euclidean_distance, output_shape=eucl_dist_output_shape)([topic_main, topic_sim3])
-dist_non = Lambda(euclidean_distance, output_shape=eucl_dist_output_shape)([topic_main, topic_nonsim1])
+dist1 = Lambda(euclidean_distance, output_shape=eucl_dist_output_shape)(
+    [topic_main, topic_sim1])
+dist2 = Lambda(euclidean_distance, output_shape=eucl_dist_output_shape)(
+    [topic_main, topic_sim2])
+dist3 = Lambda(euclidean_distance, output_shape=eucl_dist_output_shape)(
+    [topic_main, topic_sim3])
+dist_non = Lambda(euclidean_distance, output_shape=eucl_dist_output_shape)(
+    [topic_main, topic_nonsim1])
 
 decoder = get_decoder_net(hidden_dim, doc_dim)
 reconstruction = decoder(topic_main)
@@ -64,6 +66,6 @@ reconstruction = decoder(topic_main)
 model = Model(input=[main_in, sim1_in, sim2_in, sim3_in, nonsim1_in],
               output=[reconstruction, dist1, dist2, dist3, dist_non])
 
-model.compile('sgd', loss=['mse']*4 + [non_sim_loss])
+model.compile('sgd', loss=['mse'] * 4 + [non_sim_loss])
 
-#model.fit(xtrain, y_train)
+# model.fit(xtrain, y_train)
